af44f3ac9e759ab239f92e156f4eacae678d1948
abdullah <230536727+abdullah-butt-dev@users.noreply.github.com>
2025-09-27 02:17:36 +0500

    Create operate function to call other functions

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 let num1, num2, operator;
 
+
+function operate(num1, operator, num2) {
+
+  let result;
+
+  switch(operator) {
+    case '+': 
+      result = add(num1, num2);
+      break;    
+    
+    case '-':
+      result = sub(num1, num2);
+      break; 
+
+    case '*': 
+      result = mul(num1, num2);
+      break;    
+    
+    case '/':
+      result = div(num1, num2);
+      break; 
+  }
+
+  console.log(result);
+
+}
+
 function add(num1, num2) {
   return num1 + num2;
 }
